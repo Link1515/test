8eb4f486d76ee1bab91ec81cdde0fffa1fbc288f
Link1515 <h26281660@gmail.com>
2021-11-08 21:27:39 +0800

    test ajax POST

--- test_express/server.js
+++ test_express/server.js
@@ -1,19 +1,25 @@
 // 1. 引入 express
-const express = require('express');
+const express = require('express')
 
 // 2. 創建應用物件
-const app = express();
+const app = express()
 
 // 3. 創建路由規則
 // request 是對請求的封裝
 // response 是對回應的封裝
 app.get('/server', (request, response) => {
   // 設置回應頭 設置允許跨域
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.send('hello ajax');
-});
+  response.set('Access-Control-Allow-Origin', '*')
+  response.send('hello ajax')
+})
+
+app.post('/server', (request, response) => {
+  // 設置回應頭 設置允許跨域
+  response.setHeader('Access-Control-Allow-Origin', '*')
+  response.send('hello ajax POST')
+})
 
 // 4. 監聽端口啟動服務
 app.listen(8000, () => {
-  console.log('服務已經啟動, 8000端口監聽中...');
-});
+  console.log('服務已經啟動, 8000端口監聽中...')
+})
