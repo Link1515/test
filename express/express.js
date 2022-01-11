264793f493bc7507d5e41f8d3667b9cafb9f4918
Link1515 <h26281660@gmail.com>
2022-01-11 08:03:29 +0800

    20220111

--- express/express.js
+++ express/express.js
@@ -3,19 +3,18 @@ const path = require('path');
 
 const app = express();
 
-const myLogger = (req, res, next) => {
-  console.log('logged');
-  next();
-};
+app.get('/user/:id', (req, res, next) => {
+  console.log(req.originalUrl)
+  next()
+}, (req, res, next) => {
+  console.log(req.params)
+  next()
+})
 
-app.use(myLogger);
-app.get('/', (req, res) => {
-  res.send('hi');
-});
-
-app.get('/aaa', (req, res) => {
-  res.send('aaa');
-});
+app.get('/user/:id', (req, res) => {
+  // end 
+  res.sendStatus(200)
+})
 
 app.listen(3000, () => {
   console.log('server running');
