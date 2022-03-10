58789a8519a13ea3d34f6cc624ed3771f323e88d
Link1515 <h26281660@gmail.com>
2022-03-10 21:52:30 +0800

    test vue 3 watch

--- vue3_test/src/App.vue
+++ vue3_test/src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, reactive, watch } from 'vue';
 
 const name = ref('Link');
 const age = ref(23);
 const obj = reactive({
   data1: 123,
-  data2: 456
+  data2: 456,
+  data3: {
+    d1: 'hi',
+    d2: 'hello'
+  }
 });
 // Vue 3 可以監測陣列!
 const arr = reactive(['d1', 'd2', 'd3']);
@@ -14,8 +18,35 @@ function change() {
   name.value = 'Link2';
   age.value = 24;
   obj.data1 = 777;
+  obj.data3.d1 = 123;
   arr[0] = 'dd';
 }
+
+watch(name, () => {
+  console.log('名字改變');
+});
+
+// 都是新值
+// watch(obj, (newVal, oldVal) => {
+//   console.log(newVal, oldVal);
+// });
+
+// watch(
+//   () => obj.data1,
+//   (newVal, oldVal) => {
+//     console.log(newVal, oldVal);
+//   }
+// );
+
+watch(
+  () => obj.data3,
+  (newVal, oldVal) => {
+    console.log(newVal, oldVal);
+  },
+  {
+    deep: true
+  }
+);
 </script>
 
 <template>
